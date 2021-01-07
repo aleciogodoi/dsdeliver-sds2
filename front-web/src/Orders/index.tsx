579de569ac88b95838f967531ce63b5b8ea87e7a
Alécio <alecio.godoi@ymail.com>
2021-01-07 18:48:12 -0300

    Add dynamic cards

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,23 @@
-import StepsHeader from './StepsHeader';
 import './styles.css';
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import { Product } from './types';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    },   [] );
+
     return(
         <div className="orders-container">
-            <StepsHeader></StepsHeader>
+            <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
